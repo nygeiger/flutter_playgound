0e3f355d7cb9ec9b37fd033557679b8685f4bd36
Nyles Geiger <nygeiger@gmail.com>
2024-04-23 18:13:10 -0400

    Make Button UI more appealing

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         ),
         home: const MyHomePage(),
       ),
@@ -43,7 +43,10 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var currentWordPair = WordPair.random();
 
-  void getNext(){ notifyListeners(); currentWordPair = WordPair.random(); /*notifyListeners();*/ } // order doesn't matter
+  void getNext() {
+    notifyListeners();
+    currentWordPair = WordPair.random(); /*notifyListeners();*/
+  } // order doesn't matter
 }
 
 class MyHomePage extends StatelessWidget {
@@ -55,21 +58,29 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.currentWordPair;
 
     return Scaffold(
-      body: Column(
-        children: [
-          const Text('A random BS idea:'),
-          BigCard(pair: pair),
+      body: Center(
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+           mainAxisAlignment: MainAxisAlignment.center,        
+          children: [
+            // const Padding(padding: EdgeInsets.only(left: 450)),
+            // const Padding( padding: EdgeInsets.only(left: 100), child: Text('A random Starter App idea:')),
+            const Text('A random Starter App idea:'),
+            const SizedBox(height: 10,),
+            BigCard(pair: pair),
+            const SizedBox(height: 15,),
 
-          // Added this button
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed');
-              // appState.current = WordPair.random();  //Changes data under the hood but doesn't update ui
-              appState.getNext();
-            },
-            child: const Text('Next'),
-          )
-        ],
+            // Added this button
+            ElevatedButton(
+              onPressed: () {
+                print('button pressed');
+                // appState.current = WordPair.random();  //Changes data under the hood but doesn't update ui
+                appState.getNext();
+              },
+              child: const Text('Next'),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -85,6 +96,18 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(pair.asLowerCase);
+    final theme = Theme.of(context); // For consistency, use already defined theme
+
+    /// Calling copyWith() on displayMedium returns a copy of the text style with the changes you define.
+    final style = theme.textTheme.displayMedium!.copyWith(color: theme.colorScheme.onPrimary);                                                                                  
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        // Flutter Preffers make everything (including attributes) Widgets (elements) for seperation of responsibility
+        padding: const EdgeInsets.all(30.0),
+        child: Text(pair.asLowerCase, style: style, semanticsLabel: "${pair.first} ${pair.second}",),
+      ),
+    );
   }
 }
